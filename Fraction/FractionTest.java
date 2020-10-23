6811638d717692b7c3af1cc7725e0ff0fd5b7511
jbt-17 <70962684+jbt-17@users.noreply.github.com>
2020-10-22 21:54:09 -0400

    Add files via upload

---
+++ Fraction/FractionTest.java
@@ -0,0 +1,10 @@
+public class FractionTest{
+   public static void main(String[] args){
+      Fraction f = new Fraction();
+      Fraction f2 = new Fraction(5, 6);
+      f.add();
+      f.sub();
+      f.divide();
+      f.multiply();
+   }
+}
