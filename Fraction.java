82f8882f9ede820a776949351adbf18c48763047
jbt-17 <70962684+jbt-17@users.noreply.github.com>
2020-11-05 18:48:20 -0500

    Add files via upload

---
+++ Fraction.java
@@ -0,0 +1,62 @@
+public class Fraction{
+   private int num;
+   private int den;  
+    
+   public Fraction(){
+      num = 1;
+      den = 2;
+   }
+   
+   public Fraction(int num1, int num2){
+      num = num1;
+      den = num2;
+   }
+   
+   public static void add(int num1, int den1, int num2, int den2){
+      if(den1 == den2){
+         int result = num1 + num2; 
+         System.out.println(result + den1); 
+      }
+      
+      int newden1 = den1 * den2;
+      int newnum1 = num1 * den2; 
+      int newden2 = den1 * den2;
+      int newnum2 = num2 * den1;
+      
+      int nresult = newnum1 + newnum2;
+      System.out.println(nresult + "/" + newden1); 
+   }
+   
+   public static void sub(int num1, int den1, int num2, int den2){
+    if(den1 == den2){
+         int result = num1 - num2; 
+         System.out.println(result + "/" + den1); 
+      }
+      
+      int newden1 = den1 * den2;
+      int newnum1 = num1 * den2; 
+      int newden2 = den1 * den2;
+      int newnum2 = num2 * den1;
+      
+      int nresult = newnum1 - newnum2;
+      System.out.println(nresult + "/" + newden1);
+   }
+     
+   public static void divide(int num1, int den1, int num2, int den2){
+     num1 = 2;
+     den1 = 4;
+     
+     num2 = 6;
+     den2 = 4;
+     
+     int nresult = num1 * den2;
+     int dresult = den1 * num2;
+     System.out.println(nresult + "/" + dresult);
+   }
+   
+   public static void multiply(int num1, int den1, int num2, int den2){  
+     int nresult = num1 * num2;
+     int dresult = den1 * den2;
+     System.out.println(nresult + "/" + dresult);
+   }
+}
---
+++ FractionTest.java
@@ -0,0 +1,15 @@
+public class FractionTest{
+   public static void main(String[] args){
+      Fraction f = new Fraction();
+      Fraction f2 = new Fraction(5, 6);
+      
+      f.add(5, 2, 3, 5);
+      
+      f.sub(6, 8, 2, 5);
+      
+      f.divide(1, 2, 4, 6);
+      
+      f.multiply(1, 6, 3, 5);
+     
+   }
+}
